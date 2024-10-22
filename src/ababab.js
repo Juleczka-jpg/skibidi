f3c82ae5abb9fd392d40104ccf1c2494ec986d73
Juleczka-jpg <uT_4001@zsp3zamosc.pl>
2024-10-22 09:35:18 +0200

    zadanie 4

--- src/ababab.js
+++ src/ababab.js
@@ -1,4 +1,14 @@
 console.log()
-console.log("Zadanie 1")
+console.log("Zadanie 4 cz. 1")
 console.log("---------")
 console.log()
+let name1 = "Maxwell Wright";
+let name2 = "Raja Villarreal";
+let name3 = "Helen Richards";
+let phone1 = "(0191) 719 6495";
+let phone2 = "0866 398 2895";
+let phone3 = "0800 1111";
+let email1 = "Curabitur.egestas.nunc@nonummyac.co.uk";
+let email2 = "porsuere.vulputate@sed.com";
+let email3 = "libero@convallis.edu";
+
